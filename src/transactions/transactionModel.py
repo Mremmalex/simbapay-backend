cc661d0f0f85f9063e9f8a6c34e3f35f61b1b805
mremmalex <mremmalex2@gmail.com>
2022-06-03 15:46:33 +0100

    modifing transaction

--- src/transactions/transactionModel.py
+++ src/transactions/transactionModel.py
@@ -10,8 +10,8 @@ class Transaction(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     trans_id = db.Column(db.String, unique=True, default=nano_txn)
-    from_user = db.Column(db.Integer)
-    to_user = db.Column(db.Integer)
+    from_user = db.Column(db.String)
+    to_user = db.Column(db.String)
     amount = db.Column(db.Integer)
     currency_type = db.Column(db.String)
     transaction_type = db.Column(db.String)
